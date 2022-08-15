3099581a051d8ada823c723770dfcdc12f20f53e
Gokulcasper <ronitcasper215@gmail.com>
2022-08-15 19:28:24 +0530

    Feat : create collection in DB

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const server = express();
 
 let PORT = 5000;
 
+// (async()=>{     immediate invoke function
+// })()
+
 (async () => {
   try {
-    server.use(cors("*"));
-    server.use(express.json());
+    server.use(cors("*")); // not Access all url
+    server.use(express.json()); //string ("{"names":"["muthu","gokul"]"}") => JSON ({name:["muthu","gokul"]})
     server.get("/", (req, res) => {
       res.status(200).send("server is Running Successfully");
     });
--- shared/mongo.js
+++ shared/mongo.js
@@ -1,18 +1,22 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb"); // inside mango db have many class & function. we need only MongoClient class
 const DB_URL =
   "mongodb+srv://muthu:muthu123@cluster1.q9tm6.mongodb.net/gokul_posts?retryWrites=true&w=majority";
 const DB_NAME = "gokul_posts";
 
-const client = new MongoClient(DB_URL);
+const client = new MongoClient(DB_URL); // obj literals
+// Here, client using maily two functions are,
+// 1. client.connect() => connect db with server
+// 2. client.db() => for create db
 
 const mongo = {
   db: null,
-  g_posts: null,
+  g_posts: null, // collections // initially empty
 
   async connect() {
-    await client.connect();
-    this.db = client.db(DB_NAME);
-    this.g_posts = this.db.collection("g_posts");
+    await client.connect(); // connect db with server
+    this.db = client.db(DB_NAME); // create new DB ("DB name")
+    // create collection using new db
+    this.g_posts = this.db.collection("g_posts"); // create collection in g_post
     console.log("DB is Connected");
   },
 };
