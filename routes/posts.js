75be623e6c4cd98983f56d2cf1ef221f8d27f0b4
Gokulcasper <ronitcasper215@gmail.com>
2022-08-15 22:54:13 +0530

    Feat : get all post

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const db = require('../shared/mongo');
 const postApi = require("express").Router();
 
 postApi.post("/", async (req, res) => {
+  console.log("in Post")
   try {
     const post_data = req.body;
     await db.g_posts.insertOne(post_data);
@@ -10,6 +11,20 @@ postApi.post("/", async (req, res) => {
     res.status(404).send({
       message: error.message,
       error_message: "post is not created"
+    })
+  }
+})
+
+
+postApi.get("/", async (req, res) => {
+  console.log("in find all post")
+  try {
+    const all_post = await db.g_posts.find().toArray();
+    res.status(200).send({ message: "Successfully Get all data", data: all_post });
+  } catch (error) {
+    res.status(404).send({
+      message: error.message,
+      error_message: "Error To Fetch Data"
     })
   }
 })
